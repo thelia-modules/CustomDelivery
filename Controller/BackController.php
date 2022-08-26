f3c378b0fd6f97170ac3c0c8dc91fa3d98517c6d
Franck Allimant <fallimant@openstudio.fr>
2022-08-27 01:14:56 +0200

    Fixed missing RedirectResponse::create() in SF 6

--- Controller/BackController.php
+++ Controller/BackController.php
@@ -17,10 +17,12 @@ use CustomDelivery\CustomDelivery;
 use CustomDelivery\Model\CustomDeliverySlice;
 use CustomDelivery\Model\CustomDeliverySliceQuery;
 use Propel\Runtime\Map\TableMap;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Thelia\Controller\Admin\BaseAdminController;
+use Thelia\Core\HttpFoundation\Request;
 use Thelia\Core\Security\AccessManager;
 use Thelia\Core\Security\Resource\AdminResources;
+use Thelia\Core\Template\ParserContext;
 use Thelia\Core\Translation\Translator;
 use Thelia\Model\ConfigQuery;
 use Thelia\Tools\URL;
@@ -39,9 +41,9 @@ class BackController extends BaseAdminController
     /**
      * Save slice
      *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function saveAction()
+    public function saveAction(Request $request)
     {
         $response = $this->checkAuth([], ['customdelivery'], AccessManager::UPDATE);
 
@@ -62,15 +64,13 @@ class BackController extends BaseAdminController
         $config = CustomDelivery::getConfig();
 
         try {
-            $requestData = $this->getRequest()->request;
-
-            if (0 !== $id = intval($requestData->get('id', 0))) {
+            if (0 !== $id = (int)$request->get('id', 0)) {
                 $slice = CustomDeliverySliceQuery::create()->findPk($id);
             } else {
                 $slice = new CustomDeliverySlice();
             }
 
-            if (0 !== $areaId = intval($requestData->get('area', 0))) {
+            if (0 !== $areaId = (int)$request->get('area', 0)) {
                 $slice->setAreaId($areaId);
             } else {
                 $messages[] = Translator::getInstance()->trans(
@@ -80,8 +80,8 @@ class BackController extends BaseAdminController
                 );
             }
 
-            if ($config['method'] != CustomDelivery::METHOD_WEIGHT) {
-                $priceMax = $this->getFloatVal($requestData->get('priceMax', 0));
+            if ($config['method'] !== CustomDelivery::METHOD_WEIGHT) {
+                $priceMax = $this->getFloatVal($request->get('priceMax', 0));
                 if (0 < $priceMax) {
                     $slice->setPriceMax($priceMax);
                 } else {
@@ -93,8 +93,8 @@ class BackController extends BaseAdminController
                 }
             }
 
-            if ($config['method'] != CustomDelivery::METHOD_PRICE) {
-                $weightMax = $this->getFloatVal($requestData->get('weightMax', 0));
+            if ($config['method'] !== CustomDelivery::METHOD_PRICE) {
+                $weightMax = $this->getFloatVal($request->get('weightMax', 0));
                 if (0 < $weightMax) {
                     $slice->setWeightMax($weightMax);
                 } else {
@@ -106,7 +106,7 @@ class BackController extends BaseAdminController
                 }
             }
 
-            $price = $this->getFloatVal($requestData->get('price', 0));
+            $price = $this->getFloatVal($request->get('price', 0));
             if (0 <= $price) {
                 $slice->setPrice($price);
             } else {
@@ -139,15 +139,8 @@ class BackController extends BaseAdminController
 
     protected function getFloatVal($val, $default=-1)
     {
-        if (preg_match("#^([0-9\.,]+)$#", $val, $match)) {
-            $val = $match[0];
-            if(strstr($val, ",")) {
-                $val = str_replace(".", "", $val);
-                $val = str_replace(",", ".", $val);
-            }
-            $val = floatval($val);
-
-            return $val;
+        if (preg_match("#^([\d.,]+)$#", $val, $match)) {
+            return (float) str_replace(array('.', ','), array('', '.'), $match[0]);
         }
 
         return $default;
@@ -156,9 +149,9 @@ class BackController extends BaseAdminController
     /**
      * Save slice
      *
-     * @return \Thelia\Core\HttpFoundation\Response
+     * @return Response
      */
-    public function deleteAction()
+    public function deleteAction(Request $request)
     {
         $response = $this->checkAuth([], ['customdelivery'], AccessManager::DELETE);
 
@@ -177,9 +170,7 @@ class BackController extends BaseAdminController
         $response = null;
 
         try {
-            $requestData = $this->getRequest()->request;
-
-            if (0 !== $id = intval($requestData->get('id', 0))) {
+            if (0 !== $id = (int)$request->get('id', 0)) {
                 $slice = CustomDeliverySliceQuery::create()->findPk($id);
                 $slice->delete();
                 $responseData['success'] = true;
@@ -200,7 +191,8 @@ class BackController extends BaseAdminController
     /**
      * Save module configuration
      *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @param ParserContext $parserContext
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function saveConfigurationAction()
     {
@@ -236,8 +228,9 @@ class BackController extends BaseAdminController
         }
         if ($message) {
             $form->setErrorMessage($message);
-            $this->getParserContext()->addForm($form);
-            $this->getParserContext()->setGeneralError($message);
+            $this->getParserContext()
+                ->addForm($form)
+                ->setGeneralError($message);
 
             return $this->render(
                 "module-configure",
@@ -245,7 +238,7 @@ class BackController extends BaseAdminController
             );
         }
 
-        return RedirectResponse::create(
+        return $this->generateRedirect(
             URL::getInstance()->absoluteUrl("/admin/module/" . CustomDelivery::getModuleCode())
         );
     }

c872c5987cd6ea66b292f43c471aeb951bab38a2
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2025-08-20 16:40:22 +0200

    strict type compliance

--- Form/SliceForm.php
+++ Form/SliceForm.php
@@ -30,7 +30,7 @@ class SliceForm extends BaseForm
     /**
      * @return string the name of you form. This name must be unique
      */
-    public static function getName()
+    public static function getName(): string
     {
         return "customdelivery-configuration-form";
     }

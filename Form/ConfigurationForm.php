15541e264e0ff8a03b5df2d59d79dc991d9dcb9b
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2025-08-20 16:40:00 +0200

    strict type compliance

--- Form/ConfigurationForm.php
+++ Form/ConfigurationForm.php
@@ -49,7 +49,7 @@ class ConfigurationForm extends BaseForm
     /**
      * @return string the name of you form. This name must be unique
      */
-    public static function getName()
+    public static function getName(): string
     {
         return "customdelivery-configuration-form";
     }
